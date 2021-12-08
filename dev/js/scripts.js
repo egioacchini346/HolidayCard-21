37eb9aa30b92b960e84e8f2cf68dd898c373a17e
egioacchini346 <eag346@drexel.edu>
2021-12-08 09:11:44 -0500

    adding message to animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,6 +16,24 @@ gsap.set("#Vector_52",{transformOrigin:"center"});
 gsap.set("#medium-flame",{ opacity: 0, transformOrigin:"center"});
 gsap.set("#faint-flame",{ opacity: 0, transformOrigin:"center"});
 gsap.set("#bright-flame",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#smallest-flame",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_58",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_59",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_60",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_61",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_62",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_63",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_64",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Vector_65",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#seasons-greetings",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#your-friends",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#wishing",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#logo",{ opacity: 0, transformOrigin:"center"});
+gsap.set("#Rectangle-9",{ opacity: 0, transformOrigin:"center"});
+
+
+
+
 
 const mainTL = gsap.timeline()
 
@@ -70,6 +88,29 @@ function fireplacescene(){
     tl.to("#leaf-left", {duration: .25, opacity: 1},"-=3.8")
     tl.to("#leaf-right", {duration: .25, opacity: 1},"-=1.4")
     tl.from("#dragon", {duration: .15, scale: 0, ease: "power1.In"})
+
+    //dragon lights
+
+    tl.to("#Vector_106", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_110", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_94", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_90", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_114", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_118", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_122", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_126", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_130", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_134", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_138", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_142", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_154", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_158", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_162", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_166", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_170", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_102", {duration: .1, fill:"#FFFB99"})
+    tl.to("#Vector_146", {duration: .1, fill:"#FFFB99"})
+   
 
     //clock hands spinning
     tl.to("#Vector_52", {duration: 2, x: 2, y:1.5, rotation: 45},"Sametime")
@@ -135,25 +176,49 @@ function fireplacescene(){
     tl.from("#bright-flame", {duration: .05, opacity:.15}, "flicker16")
     tl.from("#bright-flame", {duration: .05, opacity:.90}, "flicker17")
     tl.from("#bright-flame", {duration: .15, opacity:.55}, "flicker18")
-    tl.from("#bright-flame", {duration: .10, opacity:.35}, "flicker19")
-    tl.from("#bright-flame", {duration: .15, opacity:.65}, "flicker20")
+  
     //small flames flickering
-    tl.from("#Vector_58", {duration: .2, opacity: 1}, "flicker1")
-    tl.to("#Vector_58", {duration: .2, opacity: 0})
-    tl.from("#Vector_60", {duration: .1, opacity: 1}, "flicker5")
-    tl.to("#Vector_60", {duration: .3, opacity: 0})
-    tl.from("#Vector_62", {duration: .2, opacity: 1}, "flicker9")
-    tl.to("#Vector_62", {duration: .1, opacity: 0})
-    tl.from("#Vector_65", {duration: .1, opacity: 1}, "flicker12")
-    tl.to("#Vector_65", {duration: .2, opacity: 0})
-    tl.from("#Vector_59", {duration: .2, opacity: 1}, "flicker14")
-    tl.to("#Vector_59", {duration: .2, opacity: 0},)
-    tl.from("#Vector_63", {duration: .1, opacity: 1}, "flicker14")
-    tl.to("#Vector_63", {duration: .2, opacity: 0})
-    tl.from("#Vector_61", {duration: .2, opacity: 1}, "flicker15")
-    tl.to("#Vector_61", {duration: .2, opacity: 0})
-    tl.from("#Vector_64", {duration: .1, opacity: 1}, "flicker17")
-    tl.to("#Vector_64", {duration: .2, opacity: 0})
+    tl.from("#Vector_58", {duration: .6, opacity: 1}, "flicker1")
+    tl.to("#Vector_58", {duration: .5, opacity: 0})
+    tl.from("#Vector_60", {duration: .6, opacity: 1}, "flicker5")
+    tl.to("#Vector_60", {duration: .5, opacity: 0})
+    tl.from("#Vector_62", {duration: .5, opacity: 1}, "flicker9")
+    tl.to("#Vector_62", {duration: .6, opacity: 0})
+    tl.from("#Vector_65", {duration: .6, opacity: 1}, "flicker12")
+    tl.to("#Vector_65", {duration: .5, opacity: 0})
+    tl.from("#Vector_59", {duration: .5, opacity: 1}, "flicker14")
+    tl.to("#Vector_59", {duration: .6, opacity: 0})
+    tl.from("#Vector_63", {duration: .5, opacity: 1}, "flicker14")
+    tl.to("#Vector_63", {duration: .5, opacity: 0})
+    tl.from("#Vector_61", {duration: .6, opacity: 1}, "flicker15")
+    tl.to("#Vector_61", {duration: .6, opacity: 0})
+    tl.from("#Vector_64", {duration: .5, opacity: 1}, "flicker17")
+    tl.to("#Vector_64", {duration: .6, opacity: 0})
+
+    //smallest flame
+    tl.from("#smallest-flame", {duration: .15, opacity: 1}, "flicker1")
+    tl.from("#smallest-flame", {duration: .15, opacity:.85}, "flicker2")
+    tl.from("#smallest-flame", {duration: .15, opacity:.2}, "flicker3")
+    tl.from("#smallest-flame", {duration: .15, opacity:.70}, "flicker4")
+    tl.from("#smallest-flame", {duration: .15, opacity:.15}, "flicker5")
+    tl.from("#smallest-flame", {duration: .15, opacity:.90}, "flicker6")
+    tl.from("#smallest-flame", {duration: .15, opacity:.55}, "flicker7")
+    tl.from("#smallest-flame", {duration: .15, opacity:.35}, "flicker8")
+    tl.from("#smallest-flame", {duration: .15, opacity:.65}, "flicker9")
+    tl.from("#smallest-flame", {duration: .15, opacity:.2}, "flicker10")
+    tl.from("#smallest-flame", {duration: .15, opacity: 1}, "flicker11")
+    tl.from("#smallest-flame", {duration: .15, opacity: 1}, "flicker12")
+    tl.from("#smallest-flame", {duration: .15, opacity:.85}, "flicker13")
+    tl.from("#smallest-flame", {duration: .15, opacity:.2}, "flicker14")
+    tl.from("#smallest-flame", {duration: .15, opacity:.70}, "flicker15")
+    tl.from("#smallest-flame", {duration: .15, opacity:.15}, "flicker16")
+    tl.from("#smallest-flame", {duration: .15, opacity:.90}, "flicker17")
+    tl.from("#smallest-flame", {duration: .15, opacity:.55}, "flicker18")
+    tl.from("#smallest-flame", {duration: .15, opacity:.35}, "flicker19")
+    tl.from("#smallest-flame", {duration: .15, opacity:.65}, "flicker20")
+    tl.from("#smallest-flame", {duration: .15, opacity:.2}, "flicker21")
+    tl.from("#smallest-flame", {duration: .15, opacity: 1}, "flicker22")
+    tl.to("#smallest-flame", {duration: .15, opacity: 1}, "flicker22")
 
     //small candle lights flicker
     tl.from("#light", {duration: .15, opacity: 1}, "flicker1")
@@ -181,9 +246,13 @@ function fireplacescene(){
     tl.from("#light_2", {duration: .15, opacity:.15}, "flicker10")
     tl.from("#light_2", {duration: .15, opacity: 1}, "flicker11")
 
+    //message
 
-
-    
+    tl.to("#Rectangle-9", {duration: .5, opacity: 1})
+    tl.to("#seasons-greetings", {duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#wishing", {duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#your-friends", {duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
+    tl.to("#logo", {duration: .5, opacity: 1, ease: "power1.In", stagger: {from: "edges", axis: "y"}})
     return tl;
 }
 
